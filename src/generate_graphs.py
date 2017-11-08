30327dc0f65923d0c7357401fefbd8d4675a5422
Ricardo Buring <ricardo.buring@gmail.com>
2017-11-08 15:42:16 +0100

    Don't store nauty's geng/showg output in memory
    Use the output stream instead.

--- src/generate_graphs.py
+++ src/generate_graphs.py
@@ -17,17 +17,13 @@ if __name__ == '__main__':
 
     geng = subprocess.Popen((NAUTY_DIR+'geng', '-cd3', str(vertices), '%d:%d' % (edges, edges)),
                             stdout=subprocess.PIPE, stderr=FNULL)
-    showg = subprocess.check_output((NAUTY_DIR+'showg', '-e', '-l0'), stdin=geng.stdout, stderr=FNULL)
-    geng.wait()
-    showg_output = showg.decode('ascii')
-    lines = showg_output.split('\n')
+    showg = subprocess.Popen((NAUTY_DIR+'showg', '-e', '-l0'), stdin=geng.stdout, stderr=FNULL, stdout=subprocess.PIPE)
 
-    assert lines[0] == '' and lines[-1] == ''
-    del lines[0]
-    assert len(lines) % 4 == 0
-
-    num_graphs = int(len(lines)/4)
-
-    for n in range(0, num_graphs):
-        graph_encoding = lines[4*n + 2]
-        print(graph_encoding, '    g_%d_%d_%d' % (vertices, edges, n+1))
+    line_count = -1
+    count = 0
+    for line in showg.stdout:
+        if line_count % 4 == 2:
+            graph_encoding = line.decode('ascii').rstrip()
+            print(vertices, edges, '   ', graph_encoding, '    g_%d_%d_%d' % (vertices, edges, count+1))
+            count += 1
+        line_count += 1
